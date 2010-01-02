41c921927aab8640fd77ee629e0d0dd4d2a45c41
Jake Dahn <admin@jakedahn.com>
2010-01-02 02:34:24 -0600

    updating routes and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
-
-  map.resources :users
-
-  map.resources :memberships
-
-  map.resources :messages
-
-  map.resources :discussions
-
-  map.resources :groups
-
-  map.resources :discussions
-
   map.resources :user_sessions
+  map.resources :memberships
+  map.resources :groups
+  map.resources :discussions
+  map.resources :messages
   
-  map.root :discussions
+  map.root :groups
+  
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
-  
-  map.resources :user_sessions
-  map.resources :users
-  map.resources :articles
-  map.resources :comments
-  
-  map.resources :discussions
-  
+      
 end
