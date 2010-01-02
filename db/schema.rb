b4c9af00bca17ce577cddfdf9b635fd38803fa35
Jake Dahn <admin@jakedahn.com>
2010-01-02 02:59:46 -0600

    modifying group index page and adding user_id column to groups

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100102073950) do
+ActiveRecord::Schema.define(:version => 20100102085808) do
 
   create_table "discussions", :force => true do |t|
     t.string   "subject"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20100102073950) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "memberships", :force => true do |t|
